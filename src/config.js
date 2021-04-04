85ce4f9a2fc4fb639e8a9a3ddb7439a512c55ffb
yl1462 <yliang1462@hotmail.com>
2021-04-04 12:12:35 -0500

    bypassed ssl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://vshqesltgqiphx:8cddcd32baef5f6c4761c459c8deef276234717c01d6a78df24cda67152a68f1@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d1mq9r4r1m5l7n',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://vshqesltgqiphx:8cddcd32baef5f6c4761c459c8deef276234717c01d6a78df24cda67152a68f1@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d1mq9r4r1m5l7n'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://hlvobvgrehjmuz:10153a5b5ebce22a8477a7deb287ffd7772786050ee82671df027ee9880f8681@ec2-3-233-43-103.compute-1.amazonaws.com:5432/dcnc6f42l93v8a?ssl=true',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://hlvobvgrehjmuz:10153a5b5ebce22a8477a7deb287ffd7772786050ee82671df027ee9880f8681@ec2-3-233-43-103.compute-1.amazonaws.com:5432/dcnc6f42l93v8a-test?ssl=true'
 }
