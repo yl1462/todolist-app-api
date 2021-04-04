16d7b31cb328101020a8e8d82894e38665cc1339
yl1462 <yliang1462@hotmail.com>
2021-04-04 10:43:16 -0500

    updated a few packages

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://yliang1462@localhost/todolist-app',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://yliang1462@localhost/todolist-app-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://vshqesltgqiphx:8cddcd32baef5f6c4761c459c8deef276234717c01d6a78df24cda67152a68f1@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d1mq9r4r1m5l7n',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgres://vshqesltgqiphx:8cddcd32baef5f6c4761c459c8deef276234717c01d6a78df24cda67152a68f1@ec2-54-235-108-217.compute-1.amazonaws.com:5432/d1mq9r4r1m5l7n'
 }
